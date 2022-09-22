2e8cb9622d04b5525993d8705bd8772ddff7b090
FrederikGielis <r0654052@student.thomasmore.be>
2022-09-22 10:34:40 +0200

    style login

--- keyTracker/forms.py
+++ keyTracker/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from django.db import models
+from django.contrib.auth.models import User
 
 
 class UpdateKeyForm(forms.Form):
     pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.TextField(max_length=500, blank=True)
