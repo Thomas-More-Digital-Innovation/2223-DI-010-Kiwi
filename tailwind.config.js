58b843d0ede0fe3af4515465f0a686b27e8b0f23
FrederikGielis <r0654052@student.thomasmore.be>
2022-12-16 17:10:25 +0100

    dashboard done

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
         accentgreen: '#8CFF98',
         darkgreen: '#152614',
         brownsugar: '#BB7E5D',
+        warningred: '#B02E0C',
+        warningredhover: '#EC0B43',
       },
       dropShadow: {
         'accentgreen': '0 35px 35px rgb(140, 255, 152, 0.25)',
