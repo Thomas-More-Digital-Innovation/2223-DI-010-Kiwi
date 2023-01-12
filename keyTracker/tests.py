1a2290be2fa5f1c1346cec5e921892bf3caebba5
FrederikGielis <r0654052@student.thomasmore.be>
2023-01-12 23:25:07 +0100

    added test

--- keyTracker/tests.py
+++ keyTracker/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from keyTracker import models
 # Create your tests here.
+# functions that need to run when 'manage.py test' is ran, have to start wih the word 'test'
+
+
+class KeyTestCase(TestCase):
+    def setUp(self):
+        pass
+        # key = models.Key(keyHolder=test_user, isReturned=False)
+
+    def testTakeKey(self):
+        test_user = User.objects.create_user(
+            username="testUser", password="testPassword")
+
+        key = models.Key(keyHolder=test_user, isReturned=False)
+        key.save()
+        self.assertEqual(key.isReturned, False)
+        self.assertEqual(key.keyHolder, test_user)
